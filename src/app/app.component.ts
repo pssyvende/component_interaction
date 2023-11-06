68f83ff6bb85e9d7ed617e424b7c743e62fe202f
pssyvende <paulinasikacka@gmail.com>
2023-11-06 13:40:34 +0100

    event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-excercises';
+  buttonLabel = "Kliknij mnie!";
+  isDisabled = false;
+
+  onClick() {
+    alert("Kliknąłeś przycisk!");
+  }
 }
